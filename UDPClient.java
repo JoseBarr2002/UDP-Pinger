85e26bebfb2fa5dccfd3347c59a5e20d7ab90807
JJ Barrera <jfbarrera2002@gmail.com>
2023-11-01 21:33:53 -0500

    It's all fresh, clean, and running :)

---
+++ UDPClient.java
@@ -0,0 +1,91 @@
+
+/****************************************************************************************************
+ ** Jose Barrera																			       **
+ ** COSC 4360																					   **
+ ** 10-30-2023																					   **
+ ** Purpose: Modify the UDP Client code so that the client is able to send a string of sentences   ** 
+ ** in the command line. This is then sent the server, and returned from the server. Output should **
+ ** be the length of the statement in bits, the RTT in ms, and the throughput in Mbps.			   ** 
+ ****************************************************************************************************/
+
+import java.net.*;
+import java.io.*;
+
+/**
+ * Client class used to send a message to the Server.
+ */
+public class UDPClient {
+
+	/**
+	 * Entrance to the program.
+	 * 
+	 * @param args Used to read in text within quotes.
+	 */
+	public static void main(String[] args) {
+
+		// Fields
+		/** The name of the UDPServer. */
+		String serverName = "localhost";
+		// String serverName = "192.168.1.152";
+		/** Tne port number for the UDPServer. */
+		int port = 10999;
+		/** The text inputted by the user. */
+		String statement = String.join(" ", args);
+		// String statement = args[0];
+
+		// Note: The Datagram Socket closes after the try.
+		try (DatagramSocket clientSocket = new DatagramSocket()) {
+			InetAddress IPAddress = InetAddress.getByName(serverName);
+
+			// Start time
+			long startTime = System.currentTimeMillis();
+
+			// Sending the data to the Server.
+			byte[] sendData = statement.getBytes();
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+			clientSocket.send(sendPacket);
+
+			// Debugging
+			System.out.println("UDP Client says: " + statement);
+
+			// Receiving data from the Server.
+			byte[] receiveData = new byte[1024];
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			clientSocket.receive(receivePacket);
+
+			// End time
+			long endTime = System.currentTimeMillis();
+
+			// RTT
+			long longRTTms = endTime - startTime;
+
+			// Convert RTT to seconds for throughput calculation
+			double RTTSeconds = longRTTms / 1000.0; // the math was not mathing
+
+			String receivedStatement = new String(receivePacket.getData()).trim(); // add .trim
+
+			// Print out whether or not the data is recieved or not.
+			if (!statement.equals(receivedStatement)) {
+				System.out.println(
+						"\n" + "*************************************************************************************");
+				System.out.println("Received data does not match sent data.");
+				System.out.println(
+						"*************************************************************************************" + "\n");
+			} else {
+				int statementLength = statement.length() * 8;
+				double throughput = (statementLength / (RTTSeconds / 2) / 1000000.0);
+				System.out.println(
+						"\n" + "*************************************************************************************");
+				System.out.println("Statement Length: " + statementLength + " bits");
+				System.out.println("RTT: " + longRTTms + "ms");
+				System.out.printf("Throughput: %.2f Mbps%n", throughput);
+				System.out.println(
+						"*************************************************************************************" + "\n");
+			}
+
+			clientSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ UDPServer.java
@@ -0,0 +1,66 @@
+
+/******************************************************************************************************
+ ** Jose Barrera																					 **
+ ** COSC 4360																						 **
+ ** 10-30-2023																					  	 **
+ ** Purpose: Modify the UDP Server code so that the server receives a PING from the client, and then ** 
+ ** echo the given statement back to the client. (echo is the pong).								 **
+ *****************************************************************************************************/
+
+import java.net.*;
+import java.io.*;
+import java.util.Arrays;
+
+/**
+ * Class that ...
+ */
+public class UDPServer {
+
+	/**
+	 * Entrance to the program.
+	 * 
+	 * @param args Not used.
+	 */
+	public static void main(String[] args) {
+
+		try {
+			DatagramSocket serverSocket = new DatagramSocket(10999); // creates a datagram socket and binds it to port
+																		// 10999
+			byte[] receiveData = new byte[1024];
+			byte[] sendData = new byte[1024];
+
+			while (true) {
+
+				System.out.println("UDP Server waiting for client on port " + serverSocket.getLocalPort() + "...");
+
+				// Receiving data from the Client
+				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+				serverSocket.receive(receivePacket);
+				InetAddress IPAddress = receivePacket.getAddress();
+				int port = receivePacket.getPort();
+				String receivedSentence = new String(receivePacket.getData()).trim();// add .trim to ignore space in
+																						// front or back (face palm)
+
+				// PING
+				System.out.println(
+						"\n" + "*************************************************************************************");
+				System.out.println("RECEIVED: from IPAddress " +
+						IPAddress + " and from port " + port + " the data: " + receivedSentence);
+				System.out.println(
+						"*************************************************************************************" + "\n");
+				// Sending data to the Client
+				sendData = receivedSentence.getBytes();
+				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+				serverSocket.send(sendPacket);
+
+				Arrays.fill(receiveData, (byte) 0);
+				Arrays.fill(sendData, (byte) 0);
+				System.out.println();
+				serverSocket.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
